70d40990f6cfb4742cccaa34dae07e36eb1c8fb5
Raymond Sarmiento <raymond.sarmiento@ubidy.com>
2021-04-17 19:12:27 +0800

    Initial commit

---
+++ build/protoc-gen.sh
@@ -0,0 +1,8 @@
+protoc --go_out=$GOPATH/src protos/domain/*.proto
+# protoc-go-inject-tag -input=grpc/domain/jobseeker.grpc.pb.go
+# protoc-go-inject-tag -input=grpc/domain/employment_type.grpc.pb.go
+# protoc-go-inject-tag -input=grpc/domain/skill.grpc.pb.go
+
+protoc --go_out=$GOPATH/src protos/service/*.proto
+
+protoc --go-grpc_out=$GOPATH/src protos/service/*.proto
